02d5df9fc822d8ba6bead2c3e4847f22ae5d1633
khaivutri <khai.vutri@hcmut.edu.vn>
2026-07-01 13:47:30 +0700

    feat(config): implement envconfig

---
+++ internal/api/config.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+
+type Config struct {
+	AppPort 		string 		`default:"8080" envconfig:"APP_PORT"`
+
+	ServiceName 	string		`default:"bookmark_service" envconfig:"SERVICE_NAME"`
+	InstanceId 		string 		`default:"" envconfig:"INSTANCE_ID"`
+
+}
+
+// GetServiceName returns the name of the service.
+func (cfg *Config) GetServiceName() string {
+	return cfg.ServiceName
+}
+
+// GetInstanceId returns the instance ID of the service. 
+func (cfg *Config) GetInstanceId() string {
+	
+	return cfg.InstanceId
+}
+
+// NewConfig creates a new Config instance and loads the configuration from environment variables or a .env file.
+func NewConfig() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	cfg := &Config{}
+
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	//thread-safety
+	if cfg.InstanceId == "" {
+		cfg.InstanceId = uuid.New().String()
+	}else{
+		//verify if the provided instance ID is a valid UUID
+		if _, err := uuid.Parse(cfg.InstanceId); err != nil {
+			return nil, err
+		}
+	}
+
+	return cfg, nil
+}
