60a97fcd741f5c2dda5d87e515110649fbd20eec
khaivutri <khai.vutri@hcmut.edu.vn>
2026-07-20 21:52:13 +0700

    feat(hash): implement hash passowrd & compare function

---
+++ pkg/utils/hash.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+type Hasher interface {
+	Hash(password string) (string, error)
+	Compare(hash string, password string) bool
+
+}
+
+type hasher struct {
+	
+}
+
+func NewHasher() Hasher {
+	return &hasher{}
+}
+
+var ErrHashFailed = errors.New("failed to hash password")
+func (h *hasher) Hash(password string) (string, error) {
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", ErrHashFailed
+	}
+	return string(hashBytes), nil
+}
+
+func (h *hasher) Compare(hash string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) 
+	return err == nil
+}
