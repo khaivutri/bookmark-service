94ac4cc2e2e04773b12c071f671c0c23730799a6
khaivutri <khai.vutri@hcmut.edu.vn>
2026-07-28 15:58:32 +0700

    test(register): update mock test

--- internal/service/user/mocks/register.go
+++ internal/service/user/mocks/register.go
@@ -102,6 +102,24 @@ func (_m *Service) Login(ctx context.Context, userName string, password string) 
 	return r0, r1
 }
 
+// UpdateSelfInfo provides a mock function with given fields: ctx, userID, displayName, email
+func (_m *Service) UpdateSelfInfo(ctx context.Context, userID string, displayName string, email string) error {
+	ret := _m.Called(ctx, userID, displayName, email)
+
+	if len(ret) == 0 {
+		panic("no return value specified for UpdateSelfInfo")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string, string) error); ok {
+		r0 = rf(ctx, userID, displayName, email)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // NewService creates a new instance of Service. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewService(t interface {
